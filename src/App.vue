592fd5de64d7853041171d5bfd630cba69f2549a
papinomoloko1917 <varivodamax88@gmail.com>
2026-02-08 18:07:34 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from 'vue';
+let firstname = ref("");
+let lastname = ref("");
+let isDone = ref(false);
+let done = () => {
+    isDone.value = true;
+}
+let clearName = () => {
+    firstname.value = "";
+    lastname.value = "";
+}
+
+let send = ref(false);
+let sendApply = () => {
+    send.value = true;
+}
+
+</script>
+
+<template>
+    <div v-if="!isDone && !send" @click="done">
+        <button>Ввести данные пользователя</button>
+    </div>
+    <div v-else-if="isDone && !send" class="inpt">
+        <label for="first-name">Имя</label>
+        <input id="first-name" v-model="firstname">
+        <label for="last-name">Фамилия</label>
+        <input id="last-name" v-model="lastname">
+        <button @click="sendApply">Отправить</button>
+        <button @click="clearName">Удалить</button>
+    </div>
+    <div v-else>
+        <h2>
+            {{ firstname + lastname }}
+        </h2>
+    </div>
+</template>
